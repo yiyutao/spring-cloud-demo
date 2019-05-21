5ca33ae5b385c909682b1a121f27e2f9dc4fd02a
masterYI <296328516@qq.com>
2019-05-21 16:58:19 +0800

    LoadBalancerClient接口进行服务消费

---
+++ eureka-consumer/src/main/java/net/yiyutao/eurekaconsumer/controller/DcController.java
@@ -0,0 +1,30 @@
+package net.yiyutao.eurekaconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author masteryi
+ * @version 1.0
+ **/
+@RestController
+public class DcController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc(){
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+}
